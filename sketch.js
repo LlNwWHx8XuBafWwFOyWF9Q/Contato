34e6f170a344fd633ba8ca12cf09d47cb0a03a64
LlNwWHx8XuBafWwFOyWF9Q <121517337+LlNwWHx8XuBafWwFOyWF9Q@users.noreply.github.com>
2022-12-27 16:42:38 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,25 @@
+
+//
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+
+  
+}
+
+
+function draw() {
+  textSize(90);
+  text("(27) 99202-4456", 100, 100);
+  
+  drawSprites();
+ 
+}
+
+
+
